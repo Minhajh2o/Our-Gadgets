3e52c6a9759485f06417717d9bf40929a5d7d7bb
Minhajh2o <h2o.minhaj@gmail.com>
2025-07-23 19:39:18 +0600

    card style updated for small device

--- src/Routes/Carts/ProductCart.jsx
+++ src/Routes/Carts/ProductCart.jsx
@@ -7,8 +7,8 @@ const ProductCart = ({ product }) => {
             <img className="w-full h-64 sm:h-52 md:h-52 rounded-lg object-cover" src={product_image} alt={product_title} />
           </figure>
           <div className="">
-            <h2 className="card-title text-2xl line-clamp-1">{product_title}</h2>
-            <p className="text-gray-600 text-xl pt-2 pb-4">${price}</p>
+            <h2 className="card-title text-xl md:text-2xl line-clamp-1">{product_title}</h2>
+            <p className="text-gray-600 text-lg md:text-xl pt-2 pb-4">${price}</p>
             <div className="card-actions">
               <button className="btn bg-white text-purple-700 border border-purple-700 rounded-full hover:bg-purple-100">
                 Shop Now
