10f71aa00be16ce37001e5b42d8da70a51d6da21
Minhajh2o <h2o.minhaj@gmail.com>
2025-08-02 19:08:28 +0600

    dashboard bg added and padding fixed

--- src/Routes/Dashboard/DashboardWidgets.jsx
+++ src/Routes/Dashboard/DashboardWidgets.jsx
@@ -13,21 +13,27 @@ const DashboardWidgets = ({ gadgetsData }) => {
   useEffect(() => {
     if (gadgetsData) {
       setFilteredCart(
-        gadgetsData.filter((item) => (cartItems || []).includes(item.product_id))
+        gadgetsData.filter((item) =>
+          (cartItems || []).includes(item.product_id)
+        )
       );
       setFilteredWishlist(
-        gadgetsData.filter((item) => (wishlistItems || []).includes(item.product_id))
+        gadgetsData.filter((item) =>
+          (wishlistItems || []).includes(item.product_id)
+        )
       );
     }
   }, [cartItems, wishlistItems, gadgetsData]);
-
+// pt-12 pb-4 md:pt-18 lg:pt-24 md:pb-8
   return (
-    <div className="container max-w-5xl px-4 mx-auto mt-6 md:pt-12 pb-8">
-      {activeTab === "cart" ? (
-        <Cart filteredCart={filteredCart} setFilteredCart={setFilteredCart} />
-      ) : (
-        <Wishlist filteredWishlist={filteredWishlist} />
-      )}
+    <div className="bg-slate-100">
+      <div className="container max-w-5xl px-4 mx-auto pt-6 md:pt-12 pb-12 md:pb-18 lg:pb-24">
+        {activeTab === "cart" ? (
+          <Cart filteredCart={filteredCart} setFilteredCart={setFilteredCart} />
+        ) : (
+          <Wishlist filteredWishlist={filteredWishlist} />
+        )}
+      </div>
     </div>
   );
 };
